6f9538140acacbc9da5bae4098964a8583e44be1
SanuRaj-K <sanusrk29@gmail.com>
2024-05-23 10:28:40 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Landing from "./Pages/Landing";
+
+function App() {
+  return (
+    <div className="">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Button() {
+  return <div className="">hai</div>;
+}
+
+export default Button;
